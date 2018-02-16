ec89b4bc2c253f0a5cb60851b4bb2858236e43d5
Naoki Rinmous <sukareki@gmail.com>
2018-02-16 18:25:33 +0800

    coolq: fix incorrect symbol mangle

--- coolq-client/coolq.h
+++ coolq-client/coolq.h
@@ -5,5 +5,5 @@
 // Event functions exported and called by CoolQ
 // @args_size: size of argument list, in bytes
 #define CLIENT_EVENT(return_type, function_name, exported_name, args_size) \
-    __pragma(comment(linker, "/EXPORT:" #exported_name "=_" #exported_name "@" #args_size) )\
+    __pragma(comment(linker, "/EXPORT:" #exported_name "=_" #function_name "@" #args_size) )\
     extern "C" __declspec(dllexport) return_type __stdcall function_name
